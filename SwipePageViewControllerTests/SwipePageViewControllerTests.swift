d98f6786a9d5d412fed39e3a5078f1a2ddf23f3e
Francisco Javier Horrillo Sancho <fjhorrillo@gmail.com>
2014-12-17 23:52:43 +0100

    Initial Commit

---
+++ SwipePageViewControllerTests/SwipePageViewControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwipePageViewControllerTests.swift
+//  SwipePageViewControllerTests
+//
+//  Created by Francisco Javier Horrillo Sancho on 17/12/14.
+//  Copyright (c) 2014 Francisco Javier Horrillo Sancho. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwipePageViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
